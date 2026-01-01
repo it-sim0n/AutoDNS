7ded747c7f70af876bd234fc9c6e9f1db12b71ab
Simon <192660524+it-sim0n@users.noreply.github.com>
2026-01-01 07:02:15 +0330

    Update main.py

--- autodns/main.py
+++ autodns/main.py
@@ -1,33 +1,41 @@
-from pathlib import Path
-from threading import Thread
-from .utils import run, spinner
+from .banner import show_banner
+from .runner import resolve, bruteforce, collect_results
 
 
-def resolve(domain, path, resolvers):
-    p = run([
-        "puredns", "resolve",
-        path, domain,
-        "-r", resolvers
-    ])
-    msg = "puredns resolve is running"
-    return p, msg
+def main():
+    show_banner()
 
+    print("Please select the action you want to perform:")
+    print("1) Resolve subdomains from an existing list")
+    print("2) Bruteforce subdomains using a wordlist")
+    action = input("> ").strip()
 
-def bruteforce(domain, wordlist, resolvers, outfile):
-    p = run([
-        "puredns", "bruteforce",
-        wordlist, domain,
-        "-r", resolvers,
-        "--wildcard-tests", "5",
-        "--rate-limit", "1000",
-        "-w", outfile
-    ])
-    msg = "puredns bruteforce is running"
-    return p, msg
+    domain = input("Please enter the target domain: ").strip()
+    outfile = f"{domain}_pure.txt"
 
+    if action == "1":
+        sublist = input("Please provide the path to the subdomain list file: ").strip()
 
-def wait_with_spinner(proc, msg):
-    t = Thread(target=spinner, args=(proc, msg))
-    t.start()
-    proc.wait()
-    t.join()
+        print("Please specify the input type you want to use:")
+        print("1) Use a plain subdomain list")
+        print("2) Generate permutations dynamically using dnsgen")
+        itype = input("> ").strip()
+
+        proc, msg, tmp = resolve(
+            domain=domain,
+            sublist=sublist,
+            dynamic=(itype == "2")
+        )
+
+        collect_results(proc, msg, outfile, tmp)
+
+    elif action == "2":
+        wordlist = input("Please provide the path to the wordlist file: ").strip()
+
+        proc, msg = bruteforce(domain, wordlist)
+        collect_results(proc, msg, outfile)
+
+    else:
+        raise SystemExit("Invalid action selected.")
+
+    print(f"[+] Results have been saved to: {outfile}")

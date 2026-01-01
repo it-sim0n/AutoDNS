1632fc8632d2a2ca7b7f97226d32216ee60ace77
Simon <192660524+it-sim0n@users.noreply.github.com>
2026-01-01 06:02:11 +0330

    Update runner.py

--- autodns/runner.py
+++ autodns/runner.py
@@ -3,20 +3,22 @@ from threading import Thread
 from .utils import run, spinner
 from .resolvers import create_resolvers_file
 
+TMP_DNSGEN = Path("/tmp/autodns_dnsgen.txt")
+
 def resolve(domain, sublist, dynamic=False):
-    """
-    Resolve subdomains using puredns.
-    If dynamic=True, dnsgen is used to generate subdomains.
-    """
     resolvers = create_resolvers_file()
     tmp = None
 
     if dynamic:
-        tmp = Path("/tmp/autodns_dnsgen.txt")
-        # dnsgen output to tmp file
-        dnsgen_proc = run(["dnsgen", sublist, "-f", "-", ])
+        tmp = TMP_DNSGEN
+
+        # cat sub | dnsgen -f -
+        cat_proc = run(["cat", sublist])
+        dnsgen_proc = run(["dnsgen", "-f", "-"], stdin=cat_proc.stdout)
+
         with tmp.open("w") as f:
             f.write(dnsgen_proc.stdout.read())
+
         dnsgen_proc.wait()
         input_file = tmp
         msg = "dnsgen | puredns resolve running"
@@ -24,23 +26,33 @@ def resolve(domain, sublist, dynamic=False):
         input_file = sublist
         msg = "puredns resolve running"
 
-    p = run(["puredns", "resolve", str(input_file), domain, "-r", str(resolvers)])
-    return p, msg, tmp  # tmp is returned to delete later
+    p = run([
+        "puredns",
+        "resolve",
+        str(input_file),
+        domain,
+        "-r",
+        str(resolvers)
+    ])
+
+    return p, msg, tmp
+
 
 def bruteforce(domain, wordlist):
-    """
-    Bruteforce subdomains using puredns.
-    """
     resolvers = create_resolvers_file()
-    p = run(["puredns", "bruteforce", wordlist, domain, "-r", str(resolvers)])
+    p = run([
+        "puredns",
+        "bruteforce",
+        wordlist,
+        domain,
+        "-r",
+        str(resolvers)
+    ])
     msg = "puredns bruteforce running"
     return p, msg
 
+
 def collect_results(proc, msg, outfile, tmp=None):
-    """
-    Collect output from a puredns process and save to file.
-    Optionally delete temporary dnsgen file.
-    """
     t = Thread(target=spinner, args=(proc, msg))
     t.start()
 
